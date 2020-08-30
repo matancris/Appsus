84c1e9be38b4a5cc458e45aebe1b71b8800f6d4a
hilla <hilla7070@gmail.com>
2020-08-30 08:00:07 +0300

    sunday

--- apps/Keep/cmps/notes-app/NoteTxt.jsx
+++ apps/Keep/cmps/notes-app/NoteTxt.jsx
@@ -3,8 +3,11 @@
 
 export function NoteTxt(props) {
     return (
-        <blockquote contentEditable="true" suppressContentEditableWarning={true}>
-            <p >{props['keep'].info.txt}</p>
-        </blockquote>
+        <section>
+            <p>{props['keep'].info.txt}</p>
+        </section>
+        // <blockquote contentEditable="true" suppressContentEditableWarning={true}>
+        //     <p >{props['keep'].info.txt}</p>
+        // </blockquote>
     )
 }
